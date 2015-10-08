2d1ef5dd61f5aa810f934af7506b309d9ba3d819
moona41 <moona41@uwosh.edu>
2015-10-07 20:55:02 -0500

    Setup repository, created project
    created class databaseHelper

---
+++ GamePak/GamePak/DatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GamePak
+{
+    class DatabaseHelper
+    {
+
+        public DatabaseHelper()
+        {
+
+        }
+    }
+}
